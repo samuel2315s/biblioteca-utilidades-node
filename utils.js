73ffeee8d4f2104a296dabd95378420e922bc93d
samuel2315s <samuvanegasq23@gmail.com>
2026-08-20 14:23:19 -0500

    se subio el archivo de fibonacci y el de numeros romanos

--- utils.js
+++ utils.js
@@ -9,3 +9,33 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(10));
+function numeroRomano(num) {
+    const valores = [
+        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"]
+    ];
+
+    let resultado = "";
+
+    for (const [valor, simbolo] of valores) {
+        while (num >= valor) {
+            resultado += simbolo;
+            num -= valor;
+        }
+    }
+
+    return resultado;
+}
+
+console.log(numeroRomano(1999));
